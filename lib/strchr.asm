311f1a01a310b3f0f45c42ae150b135971af6538
Fossaert Thomas <thomas.fossaert@epitech.eu>
2017-03-15 17:12:12 +0100

    strchr qui fait un truc

--- lib/strchr.asm
+++ lib/strchr.asm
@@ -1,20 +1,26 @@
 [BITS 64]
 
 section .text
-	global strchr:function
+	global strchro:function
 
-strlen:
+strchro:
   push r8
   mov	r8,rsp
+
   mov rcx,0
 
 loop:
-   cmp byte [rdi + rcx], 0
-   jz end
+    cmp byte [rdi + rcx], 0
+    mov rax, 0
+    jz end
+
+    cmp byte [rdi + rcx], cl
+    mov rax, rdi
+    jz end
+
    inc rcx
    jmp loop
 
 end:
-  mov rax,rcx
   pop r8
   ret
